adc84c13785d00f7a23cbe71620f8de1743f4a6a
AlicjaDeja <s221405@student.uek.krakow.pl>
2022-03-29 13:02:47 +0200

    2022-02-29 AD

---
+++ scraper.py
@@ -0,0 +1,12 @@
+import requests
+from bs4 import BeautifulSoup
+
+url = "https://www.ceneo.pl/84804567#tab=reviews"
+
+response = requests.get(url)
+
+page_dom = BeautifulSoup(response.text, "html.parser")
+#print(page_dom.prettify())
+
+opinions = page_dom.select("div.js_product-review")
+print(type(opinions))
